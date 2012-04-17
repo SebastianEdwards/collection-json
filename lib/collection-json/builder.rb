4f122d0dbf4a8dca4a1cadb4b687288009c46242
Sebastian Edwards <s.thursday@gmail.com>
2012-04-17 17:20:24 +1200

    Use strings instead of symbols as hash keys.

--- lib/collection-json/builder.rb
+++ lib/collection-json/builder.rb
@@ -21,21 +21,21 @@ module CollectionJSON
 
     def add_item(href, data = [], links = [], &block)
       href = CollectionJSON.add_host(href)
-      collection.items << {href: href}.tap do |item|
+      collection.items << {'href' => href}.tap do |item|
         item_builder = ItemBuilder.new(data, links)
         yield(item_builder) if block_given?
-        item.merge!({data: item_builder.data}) if item_builder.data.length > 0
-        item.merge!({links: item_builder.links}) if item_builder.links.length > 0
+        item.merge!({'data' => item_builder.data}) if item_builder.data.length > 0
+        item.merge!({'links' => item_builder.links}) if item_builder.links.length > 0
       end
     end
 
     def add_query(href, rel, prompt = '', data = [], &block)
       href = CollectionJSON.add_host(href)
-      collection.queries << {href: href, rel: rel}.tap do |query|
+      collection.queries << {'href' => href, 'rel' => rel}.tap do |query|
         query_builder = QueryBuilder.new(data)
         yield(query_builder) if block_given?
-        query.merge!({prompt: prompt}) if prompt != ''
-        query.merge!({data: query_builder.data}) if query_builder.data.length > 0
+        query.merge!({'prompt' => prompt}) if prompt != ''
+        query.merge!({'data' => query_builder.data}) if query_builder.data.length > 0
       end
     end
 
@@ -43,7 +43,7 @@ module CollectionJSON
       collection.template = Hash.new.tap do |template|
         template_builder = TemplateBuilder.new(data)
         yield(template_builder) if block_given?
-        template.merge!({data: template_builder.data}) if template_builder.data.length > 0
+        template.merge!({'data' => template_builder.data}) if template_builder.data.length > 0
       end
     end
   end
@@ -58,17 +58,17 @@ module CollectionJSON
 
     def add_data(name, value = '', prompt = '')
       data << {name: name}.tap do |data|
-        data.merge!({value: value}) if value != ''
-        data.merge!({prompt: prompt}) if prompt != ''
+        data.merge!({'value' => value}) if value != ''
+        data.merge!({'prompt' => prompt}) if prompt != ''
       end
     end
 
     def add_link(href, rel, name = '', prompt = '', render = '')
       href = CollectionJSON.add_host(href)
-      links << {href: href, rel: rel}.tap do |link|
-        link.merge!({name: name}) if name != ''
-        link.merge!({prompt: prompt}) if prompt != ''
-        link.merge!({render: render}) if render != ''
+      links << {'href' => href, 'rel' => rel}.tap do |link|
+        link.merge!({'name' => name}) if name != ''
+        link.merge!({'prompt' => prompt}) if prompt != ''
+        link.merge!({'render' => render}) if render != ''
       end
     end
   end
@@ -81,9 +81,9 @@ module CollectionJSON
     end
 
     def add_data(name, value = '', prompt = '')
-      data << {name: name}.tap do |data|
-        data.merge!({value: value}) if value != ''
-        data.merge!({prompt: prompt}) if prompt != ''
+      data << {'name' => name}.tap do |data|
+        data.merge!({'value' => value}) if value != ''
+        data.merge!({'prompt' => prompt}) if prompt != ''
       end
     end
   end
@@ -96,9 +96,9 @@ module CollectionJSON
     end
 
     def add_data(name, value = '', prompt = '')
-      data << {name: name}.tap do |data|
-        data.merge!({value: value}) if value != ''
-        data.merge!({prompt: prompt}) if prompt != ''
+      data << {'name' => name}.tap do |data|
+        data.merge!({'value' => value}) if value != ''
+        data.merge!({'prompt' => prompt}) if prompt != ''
       end
     end
   end
